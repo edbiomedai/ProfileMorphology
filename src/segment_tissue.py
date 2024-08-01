8334797f0c9b5fcd60e189b5aa467450155d9918
Hugh Warden <h.b.warden@sms.ed.ac.uk>
2024-08-01 22:32:06 +0100

    fix styling

--- src/segment_tissue.py
+++ src/segment_tissue.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser, Namespace
+
 from PIL import Image
 from scematk.io import read_zarr_bin_mask, read_zarr_ubimg
 from scematk.process import Processor
@@ -7,14 +8,16 @@ from scematk.process.contrast import GammaContrast
 from scematk.process.morphology import BinaryClosing, BinaryOpening
 from scematk.segment.tissue import OtsuThresholder
 
+
 def get_cli_args() -> Namespace:
     parser = ArgumentParser()
-    parser.add_argument('--zarr-path', type=str)
-    parser.add_argument('--json-path', type=str)
-    parser.add_argument('--zarr-out', type=str)
-    parser.add_argument('--json-out', type=str)
-    parser.add_argument('--thumb-out', type=str)
+    parser.add_argument("--zarr-path", type=str)
+    parser.add_argument("--json-path", type=str)
+    parser.add_argument("--zarr-out", type=str)
+    parser.add_argument("--json-out", type=str)
+    parser.add_argument("--thumb-out", type=str)
     return parser.parse_args()
+
 
 def main() -> None:
     args = get_cli_args()
@@ -22,8 +25,7 @@ def main() -> None:
     preprocessor = Processor([RGBToGrey(), GammaContrast(2)])
     postprocessor = Processor([BinaryClosing(2), BinaryOpening(2)])
     otsu_thresholder = OtsuThresholder(
-        preprocessor=preprocessor,
-        postprocessor=postprocessor
+        preprocessor=preprocessor, postprocessor=postprocessor
     )
     tissue_mask = otsu_thresholder.fit_and_run(image)
     tissue_mask.save(args.zarr_out, args.json_out)
@@ -31,8 +33,9 @@ def main() -> None:
     thumb = tissue_mask.get_thumb()
     thumb = thumb * 255
     thumb = thumb.astype("uint8")
-    thumb = Image.fromarray(thumb)
-    thumb.save(args.thumb_out)
+    thumb_image = Image.fromarray(thumb)
+    thumb_image.save(args.thumb_out)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
